768ac7075db880e17db8f5cc24f861585cb779c9
Mario L Aguayo, Jr <mlaguayojr@gmail.com>
2019-02-03 13:54:46 -0500

    Tweaked css. Updated keypad generation. Tweaked syntax options.

--- keypad.js
+++ keypad.js
@@ -6,7 +6,6 @@
 (function($){
     
     $.fn.extend({
-
         keypad: function(options){
 
             var options = $.extend({
@@ -16,52 +15,27 @@
             }, options);
 
             var timestamp = Date.now();
+            while(true){
+                timestamp = Date.now();
+                if( $("#"+timestamp).length == 0) {
+                    break;
+                }
+            }
 
             var output = document.createElement("input");
             output.id = timestamp;
             output.type = options.type;
             output.maxLength = options.limit;
+            output.autocomplete = "off";
             output.readOnly = true;
 
+            $(this).append(output);
+            
             var container = document.createElement("div");
             container.classList = ["keypad"];
             container.style.display = "none";
 
-            output.onfocus = function(){
-                container.style.display = "block";
-            }
-            
-            if ( $(this).attr("data-desc") != undefined ) {
-                var description = document.createElement("label");
-                description.innerText = ( $(this).attr("data-desc") || "" );
-                container.appendChild(description);
-                container.appendChild(document.createElement("br"));
-                container.appendChild(document.createElement("br"));
-            }
-
-            var pad = document.createElement("div");
-
-            var numbers = this.getNumbers();
-
-            (numbers).forEach( (n, i) => {
-                var b = document.createElement("button");
-                b.value = n;
-                b.innerText = n;
-
-                b.onclick = function(){
-                    if (output.value.length == options.limit){
-                        var t = output.value.slice(1, options.limit);
-                        output.value = t;
-                    }
-                    output.value = output.value + n;
-                }
-                
-                if ( ( (i % 3) == 0) && (i >= 3) ) {
-                    pad.appendChild(document.createElement("br"));
-                }
-
-                pad.appendChild(b);
-            });
+            var pad = this.createPad(timestamp, options.limit, options.shuffle);
 
             var deleteKey = document.createElement("button");
 
@@ -80,41 +54,72 @@
             submitKey.onclick = function(){
                 container.style.display = "none";
                 $(this).defaultValue = output.value;
-                
-                if (options.shuffle){
+                if (options.shuffle == "always") {
+                    
+                    while (pad.firstChild) {
+                        pad.removeChild(pad.firstChild);
+                    }
 
-                    var temp = $.fn.getNumbers(true);
-
-                    (pad.childNodes).forEach((n, i) => {
-                        if( i < 10 ){
-                            n.value = temp[i];
-                            n.textContent = temp[i];
-
-                            n.onclick = function(){
-                                if (output.value.length == options.limit){
-                                    var t = output.value.slice(1, options.limit);
-                                    output.value = t;
-                                }
-                                output.value = output.value + temp[i];
-                            }
-                        }
-                    });
+                    pad = $.fn.createPad(timestamp, options.limit, options.shuffle);
+                    pad.appendChild(deleteKey);
+                    pad.appendChild(submitKey);
+                    container.appendChild(pad);
                 }
+
+                $(output).parent().removeClass("keypad-container");
             }
 
             pad.appendChild(deleteKey);
             pad.appendChild(submitKey);
             container.appendChild(pad);
 
-            $(this).append(output);
             $(this).append(container);
+
+            output.onfocus = function(){
+                container.style.display = "block";
+                $(output).parent().addClass("keypad-container");
+            }
+            
+            $(output).width($(container).width());
+
+            setTimeout(function(){ return true; }, 100);
         },
-        getNumbers: function(shuffled=false) {
+        createPad: function(timestamp, limit, shuffle){
+            var pad = document.createElement("div");
+            const output = document.getElementById(timestamp);
+
+            var numbers = this.getNumbers(shuffle);
+
+            (numbers).forEach( (n, i) => {
+
+                var b = document.createElement("button");
+                b.value = n;
+                b.innerText = n;
+
+                b.onclick = function(){
+                    if (output.value.length == limit){
+                        output.value = output.value.slice(1, limit);
+                    }
+                    output.value = output.value + n;
+                }
+                
+                if ( ( (i % 3) == 0) && (i >= 3) ) {
+                    pad.appendChild(document.createElement("br"));
+                }
+
+                pad.appendChild(b);
+
+            });
+
+            return pad;
+        },
+        getNumbers: function(shuffle) {
+
             var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-            if(shuffled) {
-                for (let i = 9; i > 0; i--) {
-                    const j = Math.floor(Math.random() * (i + 1));
+            if( (shuffle == "always") || shuffle ) {
+                for (let i = 0; i < 10; i++) {
+                    var j = Math.floor(Math.random() * (i + 1));
                     [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
                 }
             }
--- readme.md
+++ readme.md
@@ -1,30 +1,35 @@
 # Keypad.js
-a jQuery plugin that creates a keypad/pinpad popup.
+a jQuery (3.3.1) plugin that creates a keypad/pinpad popup.
 
 ## Files
 1. "keypad.css" contains the basic styling
 2. "keypad.js" is the script
 
-## How To
-Create your element with atleast these attributes
-
+## Getting Started
+Create your div element like normal
 ```html
-<div id="keypad1" data-desc="Password 1"></div>
+<div id="keypad1"></div>
 ```
-> data-desc(optional): describe what this keypad is for
 
 ### Import
+Import like any other javascript file
 ```html
 <script src="keypad.js"></script>
 ```
-> import script like normal javascript
 
 ### Constructor / Syntax
 ```js
 $("#test").keypad({type: "text", limit: 5, shuffle: true});
 ```
-> type: this is based off of the input type (text, password, etc.). The default is password.
 
-> limit: the max number of numbers allowed. The default is 4.
+#### Type
+Type can be either "text" or "password". By default, type will set the input type to be password.
 
-> shuffle: reassign the buttons to new random values. The default is false.
+#### Limit
+Limit can be any number. By default, the max length of the input is set to 4.
+
+#### Shuffle
+Shuffle can be a boolean or a string value.
+* true will display the keypad shuffle the buttons in a random order.
+* false will display the keypad in its original order
+* "always" will shuffle the keypad everytime after you hit the submit/check button.
